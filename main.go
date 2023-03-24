0a0d90562219b23aac240d7a79281847cae46828
pchchv <ipchchv@gmail.com>
2023-03-24 18:20:53 +0300

    Implement the function of generating a 32-bit key

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"crypto/rand"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 func hashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
@@ -10,6 +14,17 @@ func hashPassword(password string) (string, error) {
 func checkPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+func genKey() ([]byte, error) {
+	key := make([]byte, 32)
+
+	_, err := rand.Read(key)
+	if err != nil {
+		return nil, err
+	}
+
+	return key, nil
 }
 
 func main() {}
