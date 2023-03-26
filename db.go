6795e6de96f010c93551924c8b6d378ca11cd395
pchchv <ipchchv@gmail.com>
2023-03-26 12:51:46 +0300

    Add getter func

--- db.go
+++ db.go
@@ -4,3 +4,8 @@ func saver(s Secret) string {
 	// TODO: Implement data transfer to the database
 	return s.password
 }
+
+func getter(pass string) (s Secret, err error) {
+	// TODO: Implement data retrieval from the database
+	return
+}
