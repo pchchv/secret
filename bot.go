3a986db36aa348c8bee169164c03c9a5d5d23740
pchchv <ipchchv@gmail.com>
2023-03-27 11:31:09 +0300

    Add a start message

--- bot.go
+++ bot.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"reflect"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/pchchv/golog"
 )
+
+const startmessage = "Hi, I am a bot for creating one-time secret notes, to create a note send me the command /send. To retrieve a note, send /get."
 
 func tgbot() {
 	bot, err := tgbotapi.NewBotAPI(getEnvValue("TG_BOT_TOKEN"))
 	if err != nil {
 		golog.Panic(err.Error())
 	}
-
-	bot.Debug = true
 
 	golog.Info("Authorized on account %s", bot.Self.UserName)
 
@@ -21,13 +23,17 @@ func tgbot() {
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message != nil { // If we got a message
-			golog.Info("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			bot.Send(msg)
+		if update.Message == nil {
+			continue
+		}
+		// Check that a text message was received from the user
+		if reflect.TypeOf(update.Message.Text).Kind() == reflect.String && update.Message.Text != "" {
+			switch update.Message.Text {
+			case "/start":
+				//Отправлем сообщение
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, startmessage)
+				bot.Send(msg)
+			}
 		}
 	}
 }
